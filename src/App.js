c5ed0f811deacf431e3a73143573e1afe51f569b
carlosziegler <carlos.ziegler@gmail.com>
2020-05-09 08:30:05 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,76 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import Unsplash from 'unsplash-js';
+
+import Lottie from 'react-lottie';
+import loadingData from './assets/loading.json'
 import './App.css';
 
-const {app} = window.require('electron').remote;
+const { app } = window.require('electron').remote;
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>React + Electron = <span role="img" aria-label="love">😍</span></h2>
-        </div>
-        <p className="App-intro">
-          <b> Release 0.2.7 </b>
-          Version: {app.getVersion()}
-        </p>
-      </div>
-    );
+
+function App() {
+  const [search, setSearch] = useState([])
+  const [result, setResult] = useState({})
+  const [gallery, setGallery] = useState([])
+  const [error, setError] = useState([])
+  const [isLoading, setIsLoading] = useState(false)
+
+  const unsplash = new Unsplash({ accessKey: "-8jKJyWNbSd67bE-ReI7FM5xxz0mXPNu2YPg2IXzY30" });
+
+  const defaultOptions = {
+    loop: true,
+    autoplay: true,
+    animationData: loadingData,
+
+  };
+
+  const handleSearch = (e) => {
+    e.preventDefault()
+    setGallery([])
+    setIsLoading(true)
+    setTimeout(async () => {
+      try {
+        const response = await unsplash.search.photos(search, 1, 10, { orientation: "portrait" })
+        setResult(await response.json())
+      } catch (e) {
+        setError(e)
+        console.log(error)
+      }
+
+      setIsLoading(false)
+    }, 2000);
   }
+
+  useEffect(() => {
+    function dislayGallery() {
+      setGallery(result.results)
+    }
+    dislayGallery()
+  }, [result])
+
+  return (
+    < div className="App" >
+      <h1>Unsplash</h1>
+      <form onSubmit={handleSearch}>
+        <input type="text" className="search" placeholder="Search" onChange={e => { setSearch(e.target.value) }} />
+        <button type="submit" className="btn-submit">Search</button>
+      </form>
+      <section>
+        <h2>Result</h2>
+        {
+          isLoading && <div className="loading">
+            <Lottie className="lottieFile" options={defaultOptions}
+              height={"50%"}
+              width={"50%"}
+            />
+          </div>
+        }
+        {gallery && gallery.map((element) => (
+          <a href={element.urls.raw} key={element.id} target="_blank" rel="noopener noreferrer"><img className="gallery-img" src={element.urls.small} alt="" /></a>
+        ))}
+      </section>
+    </div >
+  );
 }
 
 export default App;
